9d0dd438cb0724b65af4379bfbc1f90f756d1be5
Natatashkin <cs070283sng@gmail.com>
2020-11-01 16:47:00 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-import './styles.css';
+// import './styles.css';
+import colors from './colors';
+
+const body = document.querySelector('body');
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
+let intervalId = null;
+let isActive = false;
+
+btnStart.addEventListener('click', onBtnStart);
+btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStart() {
+    if (isActive) {
+        return;
+    }
+    isActive = true;
+   intervalId = setInterval(() => {
+       changeColor(colors);
+       console.log('clicks');
+   }, 1000);
+    
+}
+
+function onBtnStop() {
+    clearInterval(intervalId);
+    isActive = false;
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function changeColor(colorsArray) {
+    const index = randomIntegerFromInterval(0, 5);
+    return body.style.backgroundColor = colorsArray[index];
+}
